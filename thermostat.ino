648f10df1265cd6c602429d39fff14da2b1d638e
Andrew Wanex <awanex2705@gmail.com>
2019-02-07 10:59:56 -0800

    initial commit

---
+++ thermostat.ino
@@ -0,0 +1,18 @@
+/*
+ *  Smart Thermostat
+ *  
+ *  NodeMCU 1.0
+ *  
+ */
+ 
+/*================================================*/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
